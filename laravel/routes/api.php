7748b6ef690acaee64b7e0238cb512c2eaa38cc1
CREATODIDAK <ppidpolreslandak@gmail.com>
2023-02-14 11:46:21 +0700

    testlast

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/berita', App\Http\Controllers\Api\Rberita::class);
