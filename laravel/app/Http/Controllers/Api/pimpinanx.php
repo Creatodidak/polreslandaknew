bfb4e6a7cfb56666dc77d649711c5ae5da8b6108
CREATODIDAK <ppidpolreslandak@gmail.com>
2023-02-18 12:17:33 +0700

    delert

--- laravel/app/Http/Controllers/Api/pimpinanx.php
+++ laravel/app/Http/Controllers/Api/pimpinanx.php
@@ -19,4 +19,15 @@ class pimpinanx extends Controller
         return response(Pimpinan::where('id', $id)->first());
     }
 
+    public function destroy($id){
+        $try = Pimpinan::where('id', $id)->delete();
+
+        if($try){
+            $res = 'ok';
+        }else{
+            $res = 'no';
+        }
+        return response(["message" => $res]);
+    }
+
 }
--- laravel/app/Models/Pimpinan.php
+++ laravel/app/Models/Pimpinan.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Pimpinan extends Model
 {
     use HasFactory;
+    protected $fillable = ['id', 'kegiatan', 'lokasi', 'jam_start', 'jam_end', 'pju', 'forkopimda', 'tomas', 'toga', 'todat', 'personil', 'rincian', 'input', 'nrp', 'foto1', 'foto2', 'foto3', 'foto4', 'foto5', 'created_at', 'updated_at'];
 }
