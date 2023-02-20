2045229c3d616c21a679883d5b418d99b570f320
CREATODIDAK <ppidpolreslandak@gmail.com>
2023-02-20 19:09:27 +0700

    Update pimpinanx.php

--- laravel/app/Http/Controllers/Api/pimpinanx.php
+++ laravel/app/Http/Controllers/Api/pimpinanx.php
@@ -31,4 +31,84 @@ class pimpinanx extends Controller
 
     }
 
+    public function store(Request $req){
+        $validator = Validator::make($request->all(), [
+            'kegiatan' => 'required',
+            'lokasi' => 'required',
+            'jam_start' => 'required',
+            'jam_end' => 'required',
+            'rincian' => 'required',
+            'file1' => 'required',
+            'file2' => 'required',
+            'file3' => 'required',
+            'file4' => 'required',
+            'file5' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response(["msg" => "no"]);
+        }else{
+            $img1 = $request->file('file1');
+            $img2 = $request->file('file2');
+            $img3 = $request->file('file3');
+            $img4 = $request->file('file4');
+            $img5 = $request->file('file5');
+            $imgN1 = time().'.'.$img1->extension();
+            $imgN2 = time().'.'.$img2->extension();
+            $imgN3 = time().'.'.$img3->extension();
+            $imgN4 = time().'.'.$img4->extension();
+            $imgN5 = time().'.'.$img5->extension();
+
+            $destinationPath = public_path('/media/vrs/pimpinan');
+            
+            $do1 = $img1->move($destinationPath, $imgN1);
+            $do2 = $img2->move($destinationPath, $imgN2);
+            $do3 = $img3->move($destinationPath, $imgN3);
+            $do4 = $img4->move($destinationPath, $imgN4);
+            $do5 = $img5->move($destinationPath, $imgN5);
+
+
+            if($do1 && $do2 && $do3 && $do4 && $do5){
+
+                $pju = explode(',', str_replace(' ,', ',', str_replace(', ', ',', $req->pju)));
+                $pers = explode(',', str_replace(' ,', ',', str_replace(', ', ',', $req->pers)));
+                $toga = explode(',', str_replace(' ,', ',', str_replace(', ', ',', $req->toga)));
+                $tomas = explode(',', str_replace(' ,', ',', str_replace(', ', ',', $req->tomas)));
+                $todat = explode(',', str_replace(' ,', ',', str_replace(', ', ',', $req->todat)));
+                $forkopim = explode(',', str_replace(' ,', ',', str_replace(', ', ',', $req->forkopim)));
+
+                $ins = Pimpinan::create([
+                    'kegiatan' => $req->kegiatan,
+                    'lokasi' => $req->lokasi,
+                    'jam_start' => $req->jam_start,
+                    'jam_end' => $req->$jam_end,
+                    'pju' => $pju,
+                    'personil' => $pers,
+                    'forkopimda' => $forkopim,
+                    'tomas' => $tomas,
+                    'toga' => $toga,
+                    'todat' => $todat,
+                    'rincian' => nl2br($req->rincian),
+                    'nrp' => '98070129',
+                    'input' => date('Y-m-d'),
+                    'foto1' => '/media/vrs/pimpinan/'.$imgN1,
+                    'foto2' => '/media/vrs/pimpinan/'.$imgN2,
+                    'foto3' => '/media/vrs/pimpinan/'.$imgN3,
+                    'foto4' => '/media/vrs/pimpinan/'.$imgN4,
+                    'foto5' => '/media/vrs/pimpinan/'.$imgN5
+                ]);
+
+                if($ins){
+                    // return redirect('/backend/inputberita')->with(['addok' => 'OK']);
+                    return response()->json(['msg' => 'ok']);
+                }else{
+                    // return redirect('/backend/inputberita')->with(['addno' => 'GAGAL INPUT BERITA DI DATABASE! HUBUNGI ADMIN!']);    
+                    return response()->json(['msg' => 'Data Gagal Diinput!']);
+                }
+            }else{
+                return response()->json(['msg' => 'Foto Gagal Diupload!']);
+            }
+        }
+    }
+
 }
