6b3dc8e512431a9f6bd4afbc090f832811574373
CREATODIDAK <ppidpolreslandak@gmail.com>
2023-02-20 16:11:41 +0700

    Update sim.blade.php

--- laravel/resources/views/sim.blade.php
+++ laravel/resources/views/sim.blade.php
@@ -574,7 +574,7 @@
               <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
         </h2>
-        <div id="accordion-color-body-6" class="hidden" aria-labelledby="accordion-color-heading-6">
+        <div id="accordion-color-body-6 flex flex-col items-center justify-center" class="hidden" aria-labelledby="accordion-color-heading-6">
             <img src="{{URL::to('/')}}/media/sim/1.jpeg" class="m-1" alt="">
             <img src="{{URL::to('/')}}/media/sim/2.jpeg" class="m-1" alt="">
             <img src="{{URL::to('/')}}/media/sim/3.jpeg" class="m-1" alt="">
