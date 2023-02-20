1ba0acc2804cfd548698133125d38c13925f1b65
CREATODIDAK <ppidpolreslandak@gmail.com>
2023-02-20 16:10:08 +0700

    sim

--- laravel/resources/views/sim.blade.php
+++ laravel/resources/views/sim.blade.php
@@ -376,7 +376,7 @@
               <span>Alur Pembuatan SIM Baru dan Perpanjangan SIM</span>
               <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
-          </h2>
+        </h2>
         <div id="accordion-color-body-5" class="hidden" aria-labelledby="accordion-color-heading-5">
             <ol class="relative border-l border-gray-700 p-5">                  
                 <li class="mb-10 ml-6">            
@@ -567,6 +567,19 @@
                     </ol>
                 </li>
             </ol>
+        </div>
+        <h2 id="accordion-color-heading-6">
+            <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left border border-b-0 focus:ring-4 focus:ring-blue-800 border-gray-700 text-gray-400  hover:bg-gray-800" data-accordion-target="#accordion-color-body-6" aria-expanded="false" aria-controls="accordion-color-body-5">
+              <span>Galeri</span>
+              <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </button>
+        </h2>
+        <div id="accordion-color-body-6" class="hidden" aria-labelledby="accordion-color-heading-6">
+            <img src="{{URL::to('/')}}/media/sim/1.jpeg" class="m-1" alt="">
+            <img src="{{URL::to('/')}}/media/sim/2.jpeg" class="m-1" alt="">
+            <img src="{{URL::to('/')}}/media/sim/3.jpeg" class="m-1" alt="">
+            <img src="{{URL::to('/')}}/media/sim/4.jpeg" class="m-1" alt="">
+            <img src="{{URL::to('/')}}/media/sim/5.jpeg" class="m-1" alt="">
         </div>
     </div>
     <script src="{{URL::to('/')}}/assets/js/glide.js"></script>
