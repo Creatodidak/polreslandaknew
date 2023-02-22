ef60e096736f448627b9e920b07b55e64b2b14b1
CREATODIDAK <ppidpolreslandak@gmail.com>
2023-02-22 23:11:26 +0700

    Update pimpinanx.php

--- laravel/app/Http/Controllers/Api/pimpinanx.php
+++ laravel/app/Http/Controllers/Api/pimpinanx.php
@@ -16,7 +16,7 @@ class pimpinanx extends Controller
     }
 
     public function list($nrp){
-        $data = Pimpinan::where('nrp', $nrp)->get();
+        $data = Pimpinan::where(['nrp' => $nrp, 'input' => date('Y-m-d')])->get();
 
         return response($data);
     }
