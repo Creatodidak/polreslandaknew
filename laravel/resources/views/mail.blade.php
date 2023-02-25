a5fd8772bfae1f5f8333702886c412428130cd84
CREATODIDAK <ppidpolreslandak@gmail.com>
2023-02-25 21:54:35 +0700

    Update mail.blade.php

--- laravel/resources/views/mail.blade.php
+++ laravel/resources/views/mail.blade.php
@@ -1,5 +1,5 @@
 <div style="text-align:center; background:#072756; color:white; ">
-    <p>HALO {{ $details['nama'] }}</p>
+    <p>HALO {{ $details['nama'] }} !</p>
     <H3>KODE OTP ANDA ADALAH</H3>
     <H1 style="color:#ffd200; padding:10px; vertival-align:middle; text-align:center"><b><span style="background:#1f1f1f; padding:20px">{{ $details['otp'] }}</span></b></H1>
     <p>Kode Ini Hanya Berlaku 1 (Satu Kali)</p>
