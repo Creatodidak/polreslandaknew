ee0a4bf98d42f1999837fdcbb9eaee2f4f3ebd17
CREATODIDAK <ppidpolreslandak@gmail.com>
2023-02-25 21:54:12 +0700

    Update mail.blade.php

--- laravel/resources/views/mail.blade.php
+++ laravel/resources/views/mail.blade.php
@@ -1,3 +1,9 @@
-<p>Halo {{ $details['nama'] }}</p>
-<H3>KODE OTP ANDA ADALAH</H3>
-<H1 style="background: red; color:white"><b>{{ $details['otp'] }}</b></H1>
+<div style="text-align:center; background:#072756; color:white; ">
+    <p>HALO {{ $details['nama'] }}</p>
+    <H3>KODE OTP ANDA ADALAH</H3>
+    <H1 style="color:#ffd200; padding:10px; vertival-align:middle; text-align:center"><b><span style="background:#1f1f1f; padding:20px">{{ $details['otp'] }}</span></b></H1>
+    <p>Kode Ini Hanya Berlaku 1 (Satu Kali)</p>
+    
+    <small>Generated On <?= date('d-m-Y H:is'); ?></small><br>
+    <small>&copy; TIK POLRES LANDAK <?= date('Y'); ?></small>
+</div>
