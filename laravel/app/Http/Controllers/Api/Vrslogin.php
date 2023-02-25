cc9e29d473679a6a2cc2672ef00c66875a6ced89
CREATODIDAK <ppidpolreslandak@gmail.com>
2023-02-25 19:55:29 +0700

    Update Vrslogin.php

--- laravel/app/Http/Controllers/Api/Vrslogin.php
+++ laravel/app/Http/Controllers/Api/Vrslogin.php
@@ -37,9 +37,9 @@ class Vrslogin extends Controller
                                     $nama = $g->nama;
                                 };
 
-                                $send = Mail::send('mail', $data, $email, $nama, function($message) {
+                                $send = Mail::send('mail', $data, function($message) {
                                    
-                                   $message->to($email, $nama)->subject('OTP Virtual Report System');
+                                   $message->to('creatodidak@gmail.com', $data->otp)->subject('OTP Virtual Report System');
                                    $message->from('reslandak.kalbar@polri.go.id','OTP SERVER POLRES LANDAK');
                                 });
 
