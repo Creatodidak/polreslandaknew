2092bbaebca1f24aafb6a40fe0a475f9708831fb
CREATODIDAK <ppidpolreslandak@gmail.com>
2023-02-28 19:05:22 +0700

    sdsd

--- laravel/app/Http/Controllers/Api/Beritax.php
+++ laravel/app/Http/Controllers/Api/Beritax.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\ApiController;
 use App\Models\Berita;
+use Carbon\Carbon;
+use Validator;
+use Image;
+use Illuminate\Support\Facades\File;
 
 class Beritax extends Controller
 {
@@ -13,5 +17,88 @@ class Beritax extends Controller
         $data = Berita::where(['create_at' => date('Y-m-d')])->get();
         // 'nrp' => $nrp, 
         return response($data, 200);
+    }
+
+    public function addberita(Request $request){
+        $validator = Validator::make($request->all(), [
+            'judul' => 'required|min:8',
+            'penulis' => 'required|min:3',
+            'isi' => 'required|min:8',
+            'kategori' => 'required|min:8',
+            'caption' => 'required|min:8',
+            'file' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+
+        if ($validator->fails()){
+            return response()->json(['msg' => $validator->errors()]);
+        }else{
+            if(Berita::where('judul', $request->judul)->count() == 0){
+                $image = $request->file('file');
+                $imageName = time().'.'.$image->extension();
+            
+                $destinationPathThumbnail = public_path('/media/berita/thumbnail');
+                $img = Image::make($image->path());
+                $img->resize(150, 150, function ($constraint) {
+                    $constraint->aspectRatio();
+                })->save($destinationPathThumbnail.'/'.$imageName);
+            
+                $destinationPath = public_path('/media/berita');
+                $do = $image->move($destinationPath, $imageName);
+
+                if($do){
+                    $ins = Berita::create([
+                        'judul' => str_replace('*', '', strtoupper($request->judul)),
+                        'link' =>  str_replace(' ', '-',strtolower(str_replace( array( '\'', '"', ',' , '.' , ';', '<', '>' ), '', $request->judul))),
+                        'penulis' => $request->penulis,
+                        'isi' => nl2br($request->isi),
+                        'kategori' => $request->kategori,
+                        'caption' => $request->caption,
+                        'foto' => '/media/berita/'.$imageName,
+                        'thumb' => '/media/berita/thumbnail/'.$imageName,
+                        'publisher' => $request->nrp,
+                        'satker' =>  $request->satker,
+                        'view' => '0',
+                        'create_at' => date('Y-m-d')
+                    ]);
+
+                    if($ins){
+                        // return redirect('/backend/inputberita')->with(['addok' => 'OK']);
+                        return response()->json(['msg' => 'ok']);
+                    }else{
+                        // return redirect('/backend/inputberita')->with(['addno' => 'GAGAL INPUT BERITA DI DATABASE! HUBUNGI ADMIN!']);    
+                        return response()->json(['msg' => 'GAGAL INPUT BERITA DI DATABASE, HUBUNGI ADMIN!']);
+                    }
+                }else{
+                    // return redirect('/backend/inputberita')->with(['addno' => 'GAGAL INPUT BERITA KARENA GAMBAR TIDAK DAPAT DIUPLOAD']);
+                    return response()->json(['msg' => 'GAMBAR GAGAL DI UPLOAD!']);
+                }
+            }else{
+                // return redirect('/backend/inputberita')->with(['addno' => 'BERITA INI SUDAH ANDA UPLOAD SEBELUMNYA!']);
+                return response()->json(['msg' => 'BERITA SUDAH PERNAH DIINPUT!']);
+            }
+        }
+    }
+
+    public function delberita($id){
+        foreach(Berita::where('id', $id)->get() as $b){
+            $thumb = $b->thumb;
+            $foto = $b->foto;
+        }
+        if (File::exists(substr($foto, 1))) {
+
+            if(File::delete(substr($foto, 1))){
+                $del = Berita::where('id', $id)->delete();
+                File::delete(substr($thumb, 1));
+                if($del){
+                    return response()->json(['msg' => 'ok'], 200);
+                }else{
+                    return response()->json(['msg' => 'Data Gagal Dihapus'], 200);
+                }
+            }else{
+                return response()->json(['msg' => 'Foto Gagal Dihapus'], 200);
+            }
+        }else{
+            return response()->json(['msg' => 'Foto Tidak Ditemukan'], 200);
+        }
     }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -18,19 +18,24 @@ $cnt = 'App\Http\Controllers\Api\\';
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/vrslogin', $cnt.'Vrslogin@login');
+Route::post('/vrslogin/validasi', $cnt.'Vrslogin@validasi');
 
 Route::apiResource('/pimpinan', $cnt.pimpinanx::class);
 Route::get('/pimpinan/get/{nrp}', $cnt.'pimpinanx@list');
 Route::get('/pimpinan/del/{id}', $cnt.'pimpinanx@destroys');
 Route::post('/pimpinan/upd/{id}', $cnt.'pimpinanx@update');
 Route::post('/pimpinan/upd/{id}', $cnt.'pimpinanx@update');
+
 Route::apiResource('/harkamtibmas', $cnt.Lapharkamtibmas::class);
 Route::get('/harkamtibmas/{kategori}/{nrp}', $cnt.'Lapharkamtibmas@kategori');
 Route::post('/harkamtibmas/{kategori}', $cnt.'Lapharkamtibmas@store');
 Route::get('/harkamtibmas/{kategori}/show/{id}', $cnt.'Lapharkamtibmas@shows');
 Route::get('/harkamtibmas/{kategori}/del/{id}', $cnt.'Lapharkamtibmas@deletes');
-Route::post('/vrslogin', $cnt.'Vrslogin@login');
-Route::post('/vrslogin/validasi', $cnt.'Vrslogin@validasi');
+
+
 Route::get('/berita/list/{nrp}', $cnt.'Beritax@list');
+Route::post('/berita/addberita', $cnt.'Beritax@addberita');
+Route::get('/berita/del/{id}', $cnt.'Beritax@delberita');
 
  
