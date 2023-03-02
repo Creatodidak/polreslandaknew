4ae45b1681f0b380be3fd8a372cb395bbad899d9
CREATODIDAK <ppidpolreslandak@gmail.com>
2023-03-02 10:45:00 +0700

    cors

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -18,32 +18,35 @@ $cnt = 'App\Http\Controllers\Api\\';
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/vrslogin', $cnt.'Vrslogin@login');
-Route::post('/vrslogin/validasi', $cnt.'Vrslogin@validasi');
 
-Route::apiResource('/pimpinan', $cnt.pimpinanx::class);
-Route::get('/pimpinan/get/{nrp}', $cnt.'pimpinanx@list');
-Route::get('/pimpinan/del/{id}', $cnt.'pimpinanx@destroys');
-Route::post('/pimpinan/upd/{id}', $cnt.'pimpinanx@update');
-Route::post('/pimpinan/upd/{id}', $cnt.'pimpinanx@update');
+Route::middleware(['cors'])->group(function () {
+    Route::post('/vrslogin', $cnt.'Vrslogin@login');
+    Route::post('/vrslogin/validasi', $cnt.'Vrslogin@validasi');
 
-Route::apiResource('/harkamtibmas', $cnt.Lapharkamtibmas::class);
-Route::get('/harkamtibmas/{kategori}/{nrp}', $cnt.'Lapharkamtibmas@kategori');
-Route::post('/harkamtibmas/{kategori}', $cnt.'Lapharkamtibmas@store');
-Route::get('/harkamtibmas/{kategori}/show/{id}', $cnt.'Lapharkamtibmas@shows');
-Route::get('/harkamtibmas/{kategori}/del/{id}', $cnt.'Lapharkamtibmas@deletes');
+    Route::apiResource('/pimpinan', $cnt.pimpinanx::class);
+    Route::get('/pimpinan/get/{nrp}', $cnt.'pimpinanx@list');
+    Route::get('/pimpinan/del/{id}', $cnt.'pimpinanx@destroys');
+    Route::post('/pimpinan/upd/{id}', $cnt.'pimpinanx@update');
+    Route::post('/pimpinan/upd/{id}', $cnt.'pimpinanx@update');
 
-Route::get('/berita/list/{nrp}', $cnt.'Beritax@list');
-Route::get('/berita/listeksternal/{nrp}', $cnt.'Beritax@listeksternal');
-Route::post('/berita/addberita', $cnt.'Beritax@addberita');
-Route::post('/berita/addberitawartawan', $cnt.'Beritax@addberitawartawan');
-Route::get('/berita/del/{id}', $cnt.'Beritax@delberita');
+    Route::apiResource('/harkamtibmas', $cnt.Lapharkamtibmas::class);
+    Route::get('/harkamtibmas/{kategori}/{nrp}', $cnt.'Lapharkamtibmas@kategori');
+    Route::post('/harkamtibmas/{kategori}', $cnt.'Lapharkamtibmas@store');
+    Route::get('/harkamtibmas/{kategori}/show/{id}', $cnt.'Lapharkamtibmas@shows');
+    Route::get('/harkamtibmas/{kategori}/del/{id}', $cnt.'Lapharkamtibmas@deletes');
 
-Route::post('/lapsit/accesslog', $cnt.'Lapsitpolres@accesslog');
-Route::get('/lapsit/list/{kategori}', $cnt.'Lapsitpolres@list');
-Route::get('/lapsit/rincian/{id}', $cnt.'Lapsitpolres@show');
-Route::post('/lapsit/add', $cnt.'Lapsitpolres@add');
-Route::get('/lapsit/del/{id}', $cnt.'Lapsitpolres@del');
+    Route::get('/berita/list/{nrp}', $cnt.'Beritax@list');
+    Route::get('/berita/listeksternal/{nrp}', $cnt.'Beritax@listeksternal');
+    Route::post('/berita/addberita', $cnt.'Beritax@addberita');
+    Route::post('/berita/addberitawartawan', $cnt.'Beritax@addberitawartawan');
+    Route::get('/berita/del/{id}', $cnt.'Beritax@delberita');
+
+    Route::post('/lapsit/accesslog', $cnt.'Lapsitpolres@accesslog');
+    Route::get('/lapsit/list/{kategori}', $cnt.'Lapsitpolres@list');
+    Route::get('/lapsit/rincian/{id}', $cnt.'Lapsitpolres@show');
+    Route::post('/lapsit/add', $cnt.'Lapsitpolres@add');
+    Route::get('/lapsit/del/{id}', $cnt.'Lapsitpolres@del');
+});
 
 
  
